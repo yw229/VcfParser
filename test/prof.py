9c400e4c025d52ada79eae725451d9309505e3a1
James Casbon <casbon@gmail.com>
2012-02-19 16:57:23 +0000

    profiling script

--- test/prof.py
+++ test/prof.py
@@ -1,15 +1,23 @@
 import vcf
 import cProfile
+import timeit
+import pstats
+import sys
 
 def parse_1kg():
-    for line in vcf.Reader(filename='1kg.vcf.gz'):
+    for line in vcf.Reader(filename='test/1kg.vcf.gz'):
         pass
 
+if len(sys.argv) == 1:
+    sys.argv.append(None)
 
-cProfile.run('parse_1kg()', '1kg.prof')
+if sys.argv[1] == 'profile':
+    cProfile.run('parse_1kg()', '1kg.prof')
+    p = pstats.Stats('1kg.prof')
+    p.strip_dirs().sort_stats('time').print_stats()
 
-import pstats
-p = pstats.Stats('1kg.prof')
-
-p.strip_dirs().sort_stats('time').print_stats()
-
+elif sys.argv[1] == 'time':
+    t = timeit.timeit('parse_1kg()',  "from __main__ import parse_1kg", number=1)
+    print t
+else:
+    print 'prof.py profile/time'
