916ccab4691781dd04616430547f24ab246e0b44
Libor Morkovsky <liborek@gmail.com>
2012-04-05 00:28:14 +0200

    add ability to load ad-hoc filters to vcf_filter.py

--- scripts/vcf_filter.py
+++ scripts/vcf_filter.py
@@ -8,10 +8,13 @@ from vcf.parser import _Filter
 
 parser = argparse.ArgumentParser(description='Filter a VCF file',
         formatter_class=argparse.RawDescriptionHelpFormatter,
+        add_help=False
         )
-parser.add_argument('input', metavar='input', type=str, nargs=1,
+parser.add_argument('-h', '--help', action='store_true',
+        help='Show this help message and exit.')
+parser.add_argument('input', metavar='input', type=str, nargs='?', default=None,
         help='File to process (use - for STDIN)')
-parser.add_argument('filters', metavar='filter', type=str, nargs='+',
+parser.add_argument('filters', metavar='filter', type=str, nargs='*', default=None,
         help='Filters to use')
 parser.add_argument('--no-short-circuit', action='store_true',
         help='Do not stop filter processing on a site if a single filter fails.')
@@ -19,31 +22,50 @@ parser.add_argument('--output', action='store', default=sys.stdout,
         help='Filename to output (default stdout)')
 parser.add_argument('--no-filtered', action='store_true',
         help='Remove failed sites')
+parser.add_argument('--local-script', action='store', default=None,
+        help='File in current working directory with filter classes (with .py)')
 
 
-if __name__ == '__main__':
     # TODO: allow filter specification by short name
     # TODO: flag that writes filter output into INFO column
     # TODO: argument use implies filter use
     # TODO: parallelize
     # TODO: prevent plugins raising an exception from crashing the script
 
-
+def main():
     # dynamically build the list of available filters
     filters = {}
     filter_help = '\n\navailable filters:'
 
-    for p in pkg_resources.iter_entry_points('vcf.filters'):
-        filt = p.load()
+    def addfilt(filt, help):
         filters[filt.name] = filt
         filt.customize_parser(parser)
-        filter_help += '\n  %s:\t%s' % (filt.name, filt.description)
-
-    parser.description += filter_help
+        help += '\n  %s:\t%s' % (filt.name, filt.description)
+    
+    for p in pkg_resources.iter_entry_points('vcf.filters'):
+        filt = p.load()
+        addfilt(filt, filter_help)
 
     # parse command line args
     args = parser.parse_args()
 
+    # add all classes from local script, if present
+    if args.local_script != None:
+        import inspect
+        module_name = args.local_script.replace('.py', '')
+        mod = __import__(module_name)
+        classes = inspect.getmembers(mod, inspect.isclass)
+        for name, cls in classes:
+            addfilt(cls, filter_help)
+
+    parser.description += filter_help
+    
+    # show help after loading of the local module
+    # so it can include the additional options
+    if args.help or len(args.filters) == 0 or args.input == None:
+        parser.print_help()
+        parser.exit()
+        
     inp = vcf.Reader(file(args.input[0]))
 
     # build filter chain
@@ -69,4 +91,4 @@ if __name__ == '__main__':
         if (not args.no_filtered) or (record.FILTER == '.'):
             oup.write_record(record)
 
-
+if __name__ == '__main__': main()
